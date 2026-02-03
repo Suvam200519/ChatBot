2733fddd76f0557848969b55594be71fa337a84f
Suvam Mandal <164328241+Suvam200519@users.noreply.github.com>
2026-02-03 23:41:02 +0530

    Update API URL for generative language model

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ const chatForm = document.querySelector("#chat-form");
 
 // API setup
 const API_KEY = "AIzaSyBAikGlYda6defNrNqQezRCtdJFXolCiOw";
-const API_URL =`https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativeai.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
+
 
 
 const userData = {
