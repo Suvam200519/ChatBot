c8532905d5be928175723a04384db611d6797f89
Suvam Mandal <164328241+Suvam200519@users.noreply.github.com>
2026-02-03 23:18:55 +0530

    Update API URL to use gemini-1.5-pro-latest

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const chatForm = document.querySelector("#chat-form");
 
 // API setup
 const API_KEY = "AIzaSyD_EVVlE8UMuCj-a4KNh8pfCN9T0tZjB4g";
-const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash-latest:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro-latest:generateContent?key=${API_KEY}`;
 
 const userData = {
   message: null,
